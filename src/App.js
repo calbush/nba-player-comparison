cf8428abc7d32985c55f78dd1c2b8395c6055f25
Caleb Bush <calebmatthewbush@gmail.com>
2023-03-11 19:15:17 -0600

    Create hook for managing players

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
+import { useState } from 'react';
 import './App.css';
+import CardsGrid from './Components/CardsGrid';
+import './Components/PlayerCard'
 
 function App() {
+  const [cards, setCards] = useState([{name: 'guy'}, {name: 'foo'}, {name: 'thatoneguy'}])
+
   return (
     <div className="App">
-
+      <CardsGrid cards={cards}/>
     </div>
   );
 }
