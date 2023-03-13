143e0ef57af8aabb7763555b3db7b0f54e70a6fc
Caleb Bush <calebmatthewbush@gmail.com>
2023-03-12 23:57:09 -0500

    Add function for sending GET requests to balldontlie

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import { useState } from 'react';
 import './App.css';
 import CardsGrid from './Components/CardsGrid';
 import './Components/PlayerCard'
+import axios from 'axios';
 
 function App() {
-  const [cards, setCards] = useState([{name: 'guy'}, {name: 'foo'}, {name: 'thatoneguy'}])
+  const [cards, setCards] = useState([{first_name: 'guy', last_name: 'poop'}, {first_name: 'foo', last_name: 'bar'}, {first_name: 'unknown',last_name: 'thatoneguy'}])
+
+  axios.get('https://www.balldontlie.io/api/v1/players/237')
+    .then((response) => {
+      console.log('cool', response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  
 
   return (
     <div className="App">
