9b996b8face59f30fd1aa8b65318b8be191e76f8
Caleb Bush <calebmatthewbush@gmail.com>
2023-03-15 23:58:42 -0500

    Add search component

---
+++ src/Components/Search.js
@@ -0,0 +1,8 @@
+export default function Search(){
+    return(
+        <div>
+            <label htmlFor='search'>Search:</label>
+            <input name='search' type='text'></input>
+        </div>
+    )
+}
