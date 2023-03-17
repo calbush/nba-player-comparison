b35aab91b53bc41e649226f3f6f76325044de35c
Caleb Bush <calebmatthewbush@gmail.com>
2023-03-16 22:37:46 -0500

    Add functionality for making API call when submit button is clicked

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,8 +1,9 @@
-export default function Search(){
+export default function Search({ getPlayers, findPlayerid}){
     return(
         <div>
             <label htmlFor='search'>Search:</label>
-            <input name='search' type='text'></input>
+            <input id='search' name='search' type='text'></input>
+            <button htmlFor='search' onClick={() => getPlayers(findPlayerid(document.getElementById('search').value))}>Add Player</button>
         </div>
     )
 }
